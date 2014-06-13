64c5c1c3174878570b4f096e24a4e16ba77794f6
itbud <itbud@sina.com>
2014-06-13 10:32:20 +0800

    add: 添加国家和处理点列表接口示例

---
+++ system-list-country.php
@@ -0,0 +1,28 @@
+<?php
+
+require "http_request.php";
+require "auth.php";
+
+$dispatcher = array(
+    'category' => 'system',
+    'handler' => 'geography',
+    'action' => 'list-all-country',
+);
+
+$request_data = array(
+    'token' => $token,
+    'user_key' => $user_key
+);
+
+$api_address = $api_base . join("/", $dispatcher);
+echo $api_address;
+
+$response = rest_helper($api_address, $request_data, 'GET');
+
+header('Content-type: text/html; charset=utf-8');
+
+echo "<pre>";
+print_r($response);
+echo "</pre>";
+
+?>
